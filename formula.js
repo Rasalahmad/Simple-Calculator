886bae40f50f992c986afffa0d31414d1648ed77
Rasalahmad <rasalahmadsit@gmail.com>
2021-08-17 12:31:59 +0600

    update

---
+++ formula.js
@@ -0,0 +1,23 @@
+function insert(val) {
+    document.getElementById("output").value += val;
+}
+
+function equal() {
+    let x = document.getElementById("output").value;
+    let y = eval(x);
+    document.getElementById("output").value = y;
+}
+
+function clr() {
+    document.getElementById("output").value = "";
+}
+
+function del() {
+    var prevalue = document.getElementById("output").value;
+    document.getElementById("output").value = prevalue.substr(0, prevalue.length - 1);
+}
+
+function goback() {
+
+    window.history.back();
+}
